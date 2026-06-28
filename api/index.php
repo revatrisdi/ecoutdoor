394a51cb7c04c67ca8b1c900b77e5818b69bf8db
Reva <reva@gmail.com>
2026-06-29 00:34:55 +0700

    Enable PHP display_errors to debug silent crash

--- api/index.php
+++ api/index.php
@@ -4,4 +4,8 @@
  * Proxies requests to the Laravel application.
  */
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 require __DIR__ . '/../public/index.php';
